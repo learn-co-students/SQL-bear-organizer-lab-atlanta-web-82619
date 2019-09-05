1121b8851451789231840dd7db34fee28b6dd8e2
Mark Del Mar <mark@1000martians.com>
2019-09-05 16:32:07 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+insert into bears (name, age, gender, color, temperament, alive) values ("Mr. Chocolate", 1, "Male", "Black", "Fun", true);
+insert into bears (name, age, gender, color, temperament, alive) values ("Rowdy", 1, "Male", "Black", "Fun", true);
+insert into bears (name, age, gender, color, temperament, alive) values ("Tabitha", 1, "Male", "Black", "Fun", true);
+insert into bears (name, age, gender, color, temperament, alive) values ("Sergeant Brown", 1, "Male", "Black", "Fun", true);
+insert into bears (name, age, gender, color, temperament, alive) values ("Melissa", 1, "Male", "Black", "Fun", true);
+insert into bears (name, age, gender, color, temperament, alive) values ("Grinch", 1, "Male", "Black", "Fun", true);
+insert into bears (name, age, gender, color, temperament, alive) values ("Wendy", 1, "Male", "Black", "Fun", true);
+insert into bears (name, age, gender, color, temperament, alive) values (NULL, 1, "Male", "Black", "Fun", true);
